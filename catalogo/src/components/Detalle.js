7ca473ff6070b37fc5d323ee806c165f01fc39b6
monkeyloko <defesanti@gmail.com>
2023-09-28 23:12:01 -0300

    mejora diseño

--- catalogo/src/components/Detalle.js
+++ catalogo/src/components/Detalle.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'; // Asegúrate de crear un archivo DetalleProducto.css para los estilos
+import { useParams } from 'react-router-dom';
 import products from './listaProducts';
 import './Detalle.css'
 
 const Detalle = () => {
-    // Obtén el ID del producto de los parámetros de la URL
     const { id } = useParams();
-
-    // Aquí puedes buscar el producto con el ID en tu lista de productos
-    // Supongamos que tienes una función para buscar el producto por ID
-    // y la lista de productos se llama 'products'
     const product = products.find((product) => product.id === parseInt(id));
 
     if (!product) {
-        // Si no se encuentra el producto, puedes mostrar un mensaje de error o redirigir a una página de error
         return <div>No se encontró el producto.</div>;
     }
 
     return (
-        <div className="detalle-producto">
-            <h2>{product.name}</h2>
-            <p>Categoría: {product.category}</p>
-            <img src={product.image} alt={product.name} />
-            {/* Agrega más detalles del producto aquí */}
+        <div className="detalle-container">
+            <div className="detalle-image">
+                <img src={product.image} alt={product.name} />
+            </div>
+            <div className="detalle-info">
+                <h2>{product.name}</h2>
+                <p className="detalle-description">{product.description}</p>
+                <div className="detalle-more-info">
+                    <p>Categoría: {product.category}</p>
+                    <p>Precio: ${product.price}</p>
+                    {/* Agrega más detalles del producto aquí */}
+                </div>
+            </div>
         </div>
     );
 };
-
 
 export default Detalle;
--- catalogo/src/components/Productos.js
+++ catalogo/src/components/Productos.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-// Asegúrate de crear un archivo Productos.css para los estilos
-import Product from './Product'; // Importa el componente Product
-import products from './listaProducts'; // Importa la lista de productos
+import './Productos.css'; // Asegúrate de que la ruta al archivo CSS sea correcta
+import Product from './Product';
+import products from './listaProducts';
 import { Link } from 'react-router-dom';
-
 
 const Productos = () => {
     const [search, setSearch] = useState('');
     const [categoryFilter, setCategoryFilter] = useState('');
 
-    // Filtrar productos por nombre y categoría
     const filteredProducts = products.filter((product) => {
         return (
             product.name.toLowerCase().includes(search.toLowerCase()) &&
@@ -18,27 +16,26 @@ const Productos = () => {
     });
 
     return (
-        <div className="productos">
-            {/* Barra de búsqueda y filtro */}
+        <div className="productos-container">
             <div className="search-filter">
                 <input
                     type="text"
                     placeholder="Buscar productos..."
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
+                    className="search-input"
                 />
                 <select
                     value={categoryFilter}
                     onChange={(e) => setCategoryFilter(e.target.value)}
+                    className="category-select"
                 >
                     <option value="">Filtrar por categoría</option>
                     <option value="Categoría 1">Categoría 1</option>
                     <option value="Categoría 2">Categoría 2</option>
-                    {/* Agrega más categorías según sea necesario */}
                 </select>
             </div>
 
-            {/* Lista de productos */}
             <div className="product-grid">
                 {filteredProducts.map((product) => (
                     <Link to={`/detalle/${product.id}`} key={product.id}>
